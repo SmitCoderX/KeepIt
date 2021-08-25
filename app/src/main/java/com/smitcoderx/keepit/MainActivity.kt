306a3dbb8c593f4a9ba2c8053b4a451ff3a350ba
SmitCoderX <anirudh16june@gmail.com>
2021-08-25 23:56:55 +0530

    Initial Commit

---
+++ app/src/main/java/com/smitcoderx/keepit/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.smitcoderx.keepit
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.smitcoderx.keepit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
