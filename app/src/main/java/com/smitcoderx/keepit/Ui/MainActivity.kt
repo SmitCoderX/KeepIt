5d1f701001a992dc3423a3205caa79ca80e591e0
smitcoderx <anirudh16june@gmail.com>
2021-09-21 23:41:52 +0530

    Added Custom Toolbar

--- app/src/main/java/com/smitcoderx/keepit/Ui/MainActivity.kt
+++ app/src/main/java/com/smitcoderx/keepit/Ui/MainActivity.kt
@@ -15,11 +15,28 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var isOpen = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val toolbar = binding.toolbar.customToolbar
+        setSupportActionBar(toolbar)
+
+        val ivSearch = binding.toolbar.ivSearch
+        ivSearch.setOnClickListener {
+            if (!isOpen) {
+                binding.toolbar.etSearch.visibility = View.VISIBLE
+                isOpen = true
+            } else {
+                binding.toolbar.etSearch.visibility = View.GONE
+                isOpen = false
+            }
+
+        }
+
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
